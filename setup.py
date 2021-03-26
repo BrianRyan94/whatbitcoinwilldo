5905b316a9ff196ff99bd4685ff163c04b95fc04
BrianRyan94 <brian.ryan@ucdconnect.ie>
2021-03-26 11:40:01 +0000

    Added setup and requirements script

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages 
+  
+with open('requirements.txt') as f: 
+    requirements = f.readlines() 
+  
+long_description = "Package for creating secondary features, analysing and making price movement forecasts using raw bitcoin market data."
+  
+setup( 
+        name ='whatbitcoinwilldo', 
+        version ='1.0.0', 
+        author ='Brian Ryan', 
+        author_email ='brian.ryan@ucdconnect.ie', 
+        url ='https://github.com/BrianRyan94/whatbitcoinwilldo', 
+        description ='Bitcoin analysis using raw exchange market data.', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT', 
+        packages = find_packages(),  
+        keywords ='bitcoin analysis', 
+        install_requires = requirements, 
+        zip_safe = False
+) 
